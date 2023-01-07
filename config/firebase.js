c8d85f649d2fab5f62ec2cdcd3d0e37f3ea1843e
tarunfy <tarunsharma8920@gmail.com>
2023-01-07 21:11:33 +0530

    fix firebase bug

--- config/firebase.js
+++ config/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGIN_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_APP_MESSAGIN_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_APP_ID,
 };
 
 // Initialize Firebase
--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 const auth = () => {
+  const { googleAuthentication, user } = useContext(AuthContext);
+  console.log(user);
   return (
     <div className="min-h-screen bg-slate-100 min-w-full flex justify-center items-center">
-      <button className="bg-white shadow-md rounded px-4 border py-2">
+      <button
+        onClick={googleAuthentication}
+        className="bg-white shadow-md rounded px-4 border py-2"
+      >
         Continue with google
       </button>
     </div>
